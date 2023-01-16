d5bdcccf1151a82431d27ba18d53dffd497c9645
Filip <filip4125@gmail.com>
2023-01-16 15:29:27 +0100

    Urađena funkcionalnost uništavanja paketa, kada se pokupe

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+    [SerializeField] float destroyDelay = 0.5f;
+
     void OnCollisionEnter2D(Collision2D otherr)
     {
         Debug.Log("Juuu");
@@ -11,11 +15,18 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D otherr)
     {
-        if (otherr.tag == "Package")
+        if (otherr.tag == "Package" && !hasPackage)
+        {
+            hasPackage = true;
+            Destroy(otherr.gameObject, destroyDelay);
             Debug.Log("Paket je preuzet");
+        }
 
 
-        else if (otherr.tag == "Customer")
+        else if (otherr.tag == "Customer" && hasPackage)
+        {
             Debug.Log("Paket je dostavljen kupcu");
+            hasPackage = false;
+        }
     }
 }
