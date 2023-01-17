0656ce7885baeb2bda467acb30058dfaae211d37
Filip <filip4125@gmail.com>
2023-01-17 09:12:01 +0100

    Urađena funkcionalnost za menjanje boje automobila, koristeći sprite render

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -7,6 +7,15 @@ public class Delivery : MonoBehaviour
 {
     bool hasPackage = false;
     [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
+
+    SpriteRenderer spriteRender;
+
+    void Start()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
 
     void OnCollisionEnter2D(Collision2D otherr)
     {
@@ -18,6 +27,7 @@ public class Delivery : MonoBehaviour
         if (otherr.tag == "Package" && !hasPackage)
         {
             hasPackage = true;
+            spriteRender.color = hasPackageColor;
             Destroy(otherr.gameObject, destroyDelay);
             Debug.Log("Paket je preuzet");
         }
@@ -25,6 +35,7 @@ public class Delivery : MonoBehaviour
 
         else if (otherr.tag == "Customer" && hasPackage)
         {
+            spriteRender.color = noPackageColor;
             Debug.Log("Paket je dostavljen kupcu");
             hasPackage = false;
         }
